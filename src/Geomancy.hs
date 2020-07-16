25b11395c89946b72871f2700b79a79597b73c5e
Alexander Bondarenko <aenor.realm@gmail.com>
2020-07-17 00:17:42 +0300

    Add Quaternion and Vec4 to root exports

--- src/Geomancy.hs
+++ src/Geomancy.hs
@@ -3,10 +3,21 @@ module Geomancy
   , mat4
   , withMat4
 
+  , Quaternion
+  , quaternion
+  , withQuaternion
+
   , Vec3
   , vec3
   , withVec3
+
+  , Vec4
+  , vec4
+  , withVec4
+
   ) where
 
 import Geomancy.Mat4 (Mat4, mat4, withMat4)
+import Geomancy.Quaternion (Quaternion, quaternion, withQuaternion)
 import Geomancy.Vec3 (Vec3, vec3, withVec3)
+import Geomancy.Vec4 (Vec4, vec4, withVec4)
